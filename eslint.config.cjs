573d614585e5adff0c8ac73af71ea02c875a321f
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-14 15:44:21 +0200

    fix ci issue

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['playwright-report/', 'test-results/', 'dist/', 'node_modules/'],
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
