0e0851f0a8044828ffaf44ca1892e360069cd557
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-14 16:17:44 +0200

    fix import error

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -23,6 +23,7 @@ module.exports = [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'error',
     },
     ignores: ['playwright-report/', 'test-results/', 'dist/', 'node_modules/'],
   },
