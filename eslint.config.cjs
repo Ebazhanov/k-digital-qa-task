67f50414a4d2c89abee419a9856df90303298340
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-14 15:46:51 +0200

    an other att to fix ci

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,22 +1,29 @@
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    node: true,
-    browser: true,
-    es2021: true,
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+    ignores: ['playwright-report/', 'test-results/', 'dist/', 'node_modules/'],
   },
-  ignorePatterns: ['playwright-report/', 'test-results/', 'dist/', 'node_modules/'],
-  rules: {
-    'prettier/prettier': 'warn',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-  },
-};
+];
