93b88ed694bc8aa117dcdba50fce03c21a3284bb
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-15 22:24:17 +0200

    fix API flow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './testing/e2e/tests',
+  testDir: './testing', // changed from './testing/e2e/tests' to include all tests
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
