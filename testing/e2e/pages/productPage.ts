e6400f2c1f96d08e23272e40ffa8d9498bde2dc0
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2025-09-17 17:02:46 +0200

    autoformatting

--- testing/e2e/pages/productPage.ts
+++ testing/e2e/pages/productPage.ts
@@ -8,7 +8,7 @@ export const ProductPage = (page: Page) => {
   return {
     productWishListHeartIconById: (productId: string) =>
       page.locator(`[data-testid="p-id-${productId}"] [data-testid="wishlistHeart"]`),
-    productWishListSelectedIconById: (productId: string) =>page
-    .locator('[data-testid="p-id-60406729"] [data-testid="wishlistHeartFilled"]')
+    productWishListSelectedIconById: (productId: string) =>
+      page.locator('[data-testid="p-id-60406729"] [data-testid="wishlistHeartFilled"]'),
   };
 };
